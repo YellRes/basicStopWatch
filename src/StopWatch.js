822a97939a54ac459161dc184a2eb94b9a646087
Yellres <2929799602@qq.com>
2020-12-24 22:34:37 +0800

    fix: 合并代码报错

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -20,6 +20,32 @@ class StopWatch extends React.Component {
             splits: [...this.state.splits, this.state.currentTime - this.state.startTime]
         })
     }
+
+    onStart = () => {
+        this.setState({
+            isStarted: true,
+            startTime: new Date(),
+            currentTime: new Date(),
+        });
+      
+        this.intervalHandle = setInterval(() => {
+        this.setState({currentTime: new Date()});
+        }, 1000 / 60);
+    }
+    onPause = () => {
+        clearInterval(this.intervalHandle);
+        this.setState({
+          isStarted: false,
+        });
+      }
+    
+      onReset = () => {
+        this.setState({
+          startTime: null,
+          currentTime: null,
+          splits: [],
+        });
+      }
     render () {
         return (
             <Fragment>
